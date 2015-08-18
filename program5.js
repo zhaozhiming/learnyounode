441b2e20775ad7019971cb599a017683144e3852
zhaozhiming <kingzzm1982@sina.com>
2015-08-18 18:33:02 +0800

    list filter

---
+++ program5.js
@@ -0,0 +1,14 @@
+var fs = require('fs')
+var path = require('path')
+
+function listFiles() {
+    fs.readdir(process.argv[2], function(err, list) {
+        list.forEach(function (file) {
+            if(path.extname(file) === '.' + process.argv[3]) {
+                console.log(file)
+            }
+        })
+    })
+}
+
+listFiles()
